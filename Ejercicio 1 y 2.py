0834cfb53ce1bda3b18c26c5c2abfd3d818732b9
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-15 12:38:12 +0200

    Segundo Commit

--- Ejercicio 1 y 2.py
+++ Ejercicio 1 y 2.py
@@ -1,7 +1,9 @@
-nombre = input ("Introduzca un nombre: ")
-Nota1 = float (input ("Introduzca una nota: "))
-Nota2 = float (input ("Introduzca otra nota: "))
-media = (Nota1+Nota2)/2
-Aprueba = (media>=5)
-print ("La nota media de",nombre,"es",media)
-print ("Aprueba la asignatura:",Aprueba)
+nombre = input ("Introduzca un nombre: ")           #INPUT NOMBRE (STR)
+Nota1 = float (input ("Introduzca una nota: "))     #INPUT NOTA 1 (FLOAT)
+Nota2 = float (input ("Introduzca otra nota: "))    #INPUT NOTA 2 (FLOAT)
+
+media = (Nota1+Nota2)/2     #CALCULO MEDIA (FLOAT)
+Aprueba = (media>=5)        #BOOLEAN CONDICIÓN: MEDIA>=5
+
+print ("La nota media de",nombre,"es",media)        #OUTPUT
+print ("Aprueba la asignatura:",Aprueba)        #OUTPUT
