c42e18aaa81e86e840e8336b80a27c3f8f40e886
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-15 12:36:29 +0200

    Initial commit

---
+++ Ejercicio 1 y 2.py
@@ -0,0 +1,7 @@
+nombre = input ("Introduzca un nombre: ")
+Nota1 = float (input ("Introduzca una nota: "))
+Nota2 = float (input ("Introduzca otra nota: "))
+media = (Nota1+Nota2)/2
+Aprueba = (media>=5)
+print ("La nota media de",nombre,"es",media)
+print ("Aprueba la asignatura:",Aprueba)
