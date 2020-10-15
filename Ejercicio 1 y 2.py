106182764548da822002f5390bb6970730a78863
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-15 12:49:19 +0200

    Segundo Commit

--- Ejercicio 1 y 2.py
+++ Ejercicio 1 y 2.py
@@ -1,3 +1,7 @@
+dia = int (input("Introduzca el día: "))
+mes = int (input("Introduzca el mes: "))
+anyo = int (input("Introduzca el año: "))
+
 nombre = input ("Introduzca un nombre: ")           #INPUT NOMBRE (STR)
 Nota1 = float (input ("Introduzca una nota: "))     #INPUT NOTA 1 (FLOAT)
 Nota2 = float (input ("Introduzca otra nota: "))    #INPUT NOTA 2 (FLOAT)
@@ -5,5 +9,7 @@ Nota2 = float (input ("Introduzca otra nota: "))    #INPUT NOTA 2 (FLOAT)
 media = (Nota1+Nota2)/2     #CALCULO MEDIA (FLOAT)
 Aprueba = (media>=5)        #BOOLEAN CONDICIÓN: MEDIA>=5
 
-print ("La nota media de",nombre,"es",media)        #OUTPUT
-print ("Aprueba la asignatura:",Aprueba)        #OUTPUT
+#AJUSTE POR DEFECTO: END="\n" Y SET=" ".
+print ("La nota media de",nombre,"es",media,end=". ")        #OUTPUT
+print ("Promoción:",Aprueba)        #OUTPUT
+print ("A fecha de ",dia,mes,anyo,sep="/")
